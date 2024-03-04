1bd1ca9f5da24fc2c6b78bb7f9ce2db2c8957f23
otegaa <ekiugbotega@gmail.com>
2024-03-04 01:38:23 +0100

    Fix font for page

--- components/Font.tsx
+++ components/Font.tsx
@@ -1,15 +1,27 @@
+'use client';
+import { SetStateAction, useState } from 'react';
+
 const Font = () => {
+  const [selectedFont, setSelectedFont] = useState('inter');
+
+  const handleChangeFont = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSelectedFont(e.target.value);
+  };
   return (
     <select
       name="font"
-      className="select select-bordered select-[#808080] select-xs max-w-xs focus:outline-none"
+      className={`select select-bordered select-[#808080] select-xs max-w-xs focus:outline-none font-${selectedFont}`}
+      value={selectedFont}
+      onChange={handleChangeFont}
     >
       <option disabled selected>
         Select font
       </option>
-      <option value="serif">Serif</option>
-      <option value="sans-serif"> Sans Serif</option>
-      <option value="monospace">Monospace</option>
+      <option value="inter">Inter</option>
+      <option value="inconsolata"> Inconsolata</option>
+      <option value="lora">Lora</option>
     </select>
   );
 };
