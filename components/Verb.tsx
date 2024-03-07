9b8c2ce4d1e309df8e60e7e9e92d1d2ed8450860
otegaa <ekiugbotega@gmail.com>
2024-03-07 23:43:39 +0100

    Get example statement for verb meaning

--- components/Verb.tsx
+++ components/Verb.tsx
@@ -14,11 +14,12 @@ const Verb = ({ receivedData }: Prop) => {
         <h3>{verb.partOfSpeech}</h3>
         <span className="h-px bg-[#979797] grow"></span>
       </div>
-      <div>
+      <div className="flex flex-col gap-2 border-b-2 pb-8">
         <h4>Meaning</h4>
         <ul className="marker:text-[#A445ED] list-disc list-outside ml-4">
           <li>{filteredOneDefinition.definition}</li>
         </ul>
+        <p className="ml-4">"{filteredOneDefinition.example}"</p>
       </div>
     </div>
   );
