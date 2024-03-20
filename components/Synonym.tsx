91c2eeba30f05f722d54a336a31129979d7557ba
otegaa <ekiugbotega@gmail.com>
2024-03-20 23:58:48 +0100

    Get synonym page for word

--- components/Synonym.tsx
+++ components/Synonym.tsx
@@ -1,18 +1,43 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
 interface Prop {
   receivedData: Record<string, any>[];
 }
 
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
 const Synonym = ({ receivedData }: Prop) => {
+  const [synonymData, setSynonymData] = useState(null);
+
   const data = receivedData[0];
   const synonym = data?.meanings[0]?.synonyms[0];
 
+  useEffect(() => {
+    const fetchSynonymData = async () => {
+      try {
+        const res = await fetch(`${url}${synonym}`);
+        if (!res.ok) {
+          throw new Error('Failed to fetch synonym data');
+        }
+        const data = await res.json();
+        setSynonymData(data);
+      } catch (error: any) {
+        console.error(error.message);
+      }
+    };
+
+    if (synonym) {
+      fetchSynonymData();
+    }
+  }, [synonym]);
+
   return (
     <div className="flex gap-5">
-      {synonym ? (
-        <>
-          <h4>Synonym</h4>
-          <h3 className="text-[#A445ED] ">{synonym}</h3>
-        </>
+      {synonymData ? (
+        <Link href={`/synonyms/${synonym}`}>
+          <a className="text-[#A445ED]">{synonym}</a>
+        </Link>
       ) : null}
     </div>
   );
